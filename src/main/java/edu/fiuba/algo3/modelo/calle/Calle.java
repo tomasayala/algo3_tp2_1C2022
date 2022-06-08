4508bf6bd1b88ea8728b53eda1aa2417ac92d3ca
tomasayala <55297795+tomasayala@users.noreply.github.com>
2022-06-08 19:56:37 -0300

    Update Calle.java

--- src/main/java/edu/fiuba/algo3/modelo/calle/Calle.java
+++ src/main/java/edu/fiuba/algo3/modelo/calle/Calle.java
@@ -8,18 +8,18 @@ import edu.fiuba.algo3.modelo.modificador.IModificador;
 
 public class Calle implements ICalle{
     private IModificador modificador;
-    private ArrayList<ICelda> celdas;
+    private ArrayList<Celda> celdas;
 
     public void cruzarCon(IJugador jugador) {
         modificador.cruzarCon(jugador);
     }
 
-    public ICelda proximaEsquina(ICelda esquinaActual) {
+    public ICelda proximaEsquina(Celda esquinaActual) {
         // cambiar
         return this.celdas.get(0);
     }
 
-    public boolean contiene(ICelda celda) {
+    public boolean contiene(Celda celda) {
         return false;
     }
 
