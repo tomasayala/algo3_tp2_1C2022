53990937dd4f030e038c6d82787fd6b53c8a075a
franmacke <fmacke10@gmail.com>
2022-06-09 11:14:39 -0300

    Pruebas Calle

--- src/main/java/edu/fiuba/algo3/modelo/calle/Calle.java
+++ src/main/java/edu/fiuba/algo3/modelo/calle/Calle.java
@@ -10,6 +10,10 @@ public class Calle {
     private IModificador modificador;
     private ArrayList<Celda> celdas;
 
+    public Calle() {
+        this.celdas = new ArrayList<Celda>();
+    }
+
     public void agregarCelda(Celda celda) {
         celdas.add(celda);
     }
@@ -19,11 +23,17 @@ public class Calle {
     }
 
     public Celda siguienteEsquina(Celda esquinaActual) {
-    //    FALTA HACER
+        // Solo funciona para 2 celdas
+        ArrayList<Celda> buscador = celdas;
+        buscador.remove(esquinaActual);
         return this.celdas.get(0);
     }
 
     public boolean contiene(Celda celda) {
         return this.celdas.contains(celda);
+    }
+
+    public void asociarCelda(Celda celda) {
+        celdas.add(celda);
     }
 }
--- src/main/test/edu/fiuba/algo3/modelo/calle/CalleTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/calle/CalleTest.java
@@ -1,8 +1,41 @@
 package edu.fiuba.algo3.modelo.calle;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
+
+import edu.fiuba.algo3.modelo.celda.Celda;
+import edu.fiuba.algo3.modelo.jugador.Jugador;
 
 public class CalleTest {
     
-    
+    @Test
+    public void agregoDosCeldasAUnaCalleYVerificoQueLasContenga() {
+        Calle calle = new Calle();
+
+        Celda primerCelda = new Celda(0, 0);
+        Celda segundaCelda = new Celda(0, 1);
+
+        calle.agregarCelda(primerCelda);
+        calle.agregarCelda(segundaCelda);
+
+        assertEquals(calle.contiene(primerCelda), true);
+        assertEquals(calle.contiene(segundaCelda), true);
+    }
+
+    @Test
+    public void agregoDosCeldasAUnaCalleYObtengoLaSiguienteESquina() {
+        Calle calle = new Calle();
+
+        Celda primerCelda = new Celda(0, 0);
+        Celda segundaCelda = new Celda(0, 1);
+
+        calle.agregarCelda(primerCelda);
+        calle.agregarCelda(segundaCelda);
+
+        assertEquals(calle.siguienteEsquina(primerCelda), segundaCelda);
+    }
+
 }
