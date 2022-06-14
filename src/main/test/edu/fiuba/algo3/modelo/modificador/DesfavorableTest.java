00447fc3c0187349a4a297f1412df1b9acd44ca0
Guillermina Hoffmann <ghoffmann@fi.uba.ar>
2022-06-14 10:33:09 -0300

    agrego visibilidad public a tests

--- src/main/test/edu/fiuba/algo3/modelo/modificador/DesfavorableTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/modificador/DesfavorableTest.java
@@ -10,13 +10,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DesfavorableTest {
 
     @Test
-    public void cruzarCon() {}
+    public void  modificadorDesfavorableSeCruzaConJugador() {
+        //Arrange
+        Desfavorable desfavorable = new Desfavorable();
+        Celda celda = new Celda(0, 0);
+        Moto moto = new Moto();
+        Jugador jugador = new Jugador(moto);
+        jugador.asignarCeldaInicial(celda);
 
-    @Test
-    public void aplicarSorpresa() {}
+        jugador.sumarMovimientos(10);
 
+        //Act
+        desfavorable.cruzarCon(jugador);
+        //Assert
+        assertEquals(Math.round((10 * 1.25)+1), jugador.movimientos()); //Suma el 25% de los movimientos hechos + 1 movimiento
+    }
     @Test
-    public void  modificadorDesvorableSeCruzaConJugador() {
+    public void modificadorDesfavorableSeCruzaConJugadorCon10MovimientosYJugadorQuedaCon14Movimientos() {
         Desfavorable desfavorable = new Desfavorable();
         Celda celda = new Celda(0, 0);
         Moto moto = new Moto();
@@ -28,6 +38,22 @@ public class DesfavorableTest {
         desfavorable.cruzarCon(jugador);
 
         assertEquals(Math.round((10 * 1.25) + 1), jugador.movimientos()); //Suma el 25% de los movimientos hechos + 1 movimiento. Redondea 13,5 a 14
+
+    }
+
+    @Test
+    public void modificadorDesfavorableSeCruzaConJugadorConMovimientos25YJugadorQuedaCon32Movimientos() {
+        Desfavorable desfavorable = new Desfavorable();
+        Celda celda = new Celda(0, 0);
+        Moto moto = new Moto();
+        Jugador jugador = new Jugador(moto);
+        jugador.asignarCeldaInicial(celda);
+
+        jugador.sumarMovimientos(25);
+
+        desfavorable.cruzarCon(jugador);
+
+        assertEquals(32, jugador.movimientos()); //Suma el 25% de los movimientos hechos + 1 movimiento. Redondea 32.25 a 32.
 
     }
 
