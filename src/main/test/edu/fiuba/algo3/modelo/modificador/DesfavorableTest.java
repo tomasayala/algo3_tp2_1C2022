47180a8eee604796413f476baf5034c1750d4f87
Guillermina Hoffmann <ghoffmann@fi.uba.ar>
2022-06-14 10:24:56 -0300

    nuevos test Sorpresa Desfavorable

--- src/main/test/edu/fiuba/algo3/modelo/modificador/DesfavorableTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/modificador/DesfavorableTest.java
@@ -32,4 +32,36 @@ public class DesfavorableTest {
         assertEquals(Math.round((10 * 1.25)+1), jugador.movimientos()); //Suma el 25% de los movimientos hechos + 1 movimiento
     }
 
+    @Test
+    void modificadorDesfavorableSeCruzaConJugadorCon10MovimientosYJugadorQuedaCon14Movimientos() {
+        Desfavorable desfavorable = new Desfavorable();
+        Celda celda = new Celda(0, 0);
+        Moto moto = new Moto();
+        Jugador jugador = new Jugador(moto);
+        jugador.asignarCeldaInicial(celda);
+
+        jugador.sumarMovimientos(10);
+
+        desfavorable.cruzarCon(jugador);
+
+        assertEquals(Math.round((10 * 1.25) + 1), jugador.movimientos()); //Suma el 25% de los movimientos hechos + 1 movimiento. Redondea 13,5 a 14
+
+    }
+
+    @Test
+    void modificadorDesfavorableSeCruzaConJugadorConMovimientos25YJugadorQuedaCon32Movimientos() {
+        Desfavorable desfavorable = new Desfavorable();
+        Celda celda = new Celda(0, 0);
+        Moto moto = new Moto();
+        Jugador jugador = new Jugador(moto);
+        jugador.asignarCeldaInicial(celda);
+
+        jugador.sumarMovimientos(25);
+
+        desfavorable.cruzarCon(jugador);
+
+        assertEquals(32, jugador.movimientos()); //Suma el 25% de los movimientos hechos + 1 movimiento. Redondea 32.25 a 32.
+
+    }
+
 }
