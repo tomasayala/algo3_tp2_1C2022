b2bffd540849e3187670a97c4f4e96050e0605fb
franmacke <fmacke10@gmail.com>
2022-06-15 21:23:37 -0300

    Comentarios

--- src/main/java/edu/fiuba/algo3/modelo/modificador/CambioDeVehiculo.java
+++ src/main/java/edu/fiuba/algo3/modelo/modificador/CambioDeVehiculo.java
@@ -1,8 +1,14 @@
 package edu.fiuba.algo3.modelo.modificador;
 
+import edu.fiuba.algo3.modelo.calle.Cruzable;
+import edu.fiuba.algo3.modelo.celda.Celda;
+import edu.fiuba.algo3.modelo.cruzador.Cruzador;
 import edu.fiuba.algo3.modelo.jugador.Jugador;
 
+
 public class CambioDeVehiculo implements Modificador {
+
+    private Cruzador cruzador = new Cruzable();
 
     @Override
     public void cruzarCon(Jugador jugador) {
@@ -10,4 +16,8 @@ public class CambioDeVehiculo implements Modificador {
         jugador.sumarMovimientos(NUMERO_DE_MOVIMIENTOS);
     }
 
+    public Celda puedoCruzar(Celda origen, Celda destino) {
+        return this.cruzador.puedoCruzar(origen, destino);
+    }
+    
 }
